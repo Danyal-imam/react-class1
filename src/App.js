83abbf2f9652db77742e52d26d51fe1a934e274e
Danyal Imam <danyal.imam94@gmail.com>
2020-06-11 09:28:40 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Danyal Imam</h1>
+    <p>This is react assigent#1</p>
+    </div>
+  );
+}
+
+export default App;
